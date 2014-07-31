c890d6e370c88c0a3e19dd4f224626b7369064b8
Tristan Kuehn <tristankuehn@gmail.com>
2014-07-31 17:37:58 -0400

    Some WIP front end files.

---
+++ Front_End/generate_graph.js
@@ -0,0 +1,18 @@
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawChart);
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Year', 'Sales', 'Expenses'],
+        ['2004',  1000,      400],
+        ['2005',  1170,      460],
+        ['2006',  660,       1120],
+        ['2007',  1030,      540]
+    ]);
+
+    var options = {
+        title: 'Company Performance'
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
---
+++ Front_End/show_graph.php
@@ -0,0 +1,25 @@
+<?php include 'PHP_source.php'>
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+      google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart)
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          //PHP database calls go here
+        ]);
+
+        var options = {
+          title: 'Sensor Data'
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="chart_div" style="width: 900px; height: 500px;"></div>
+  </body>
+</html>
