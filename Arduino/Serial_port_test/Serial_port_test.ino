ea2a6a3fdb228288da04f16ddf333e522ca1279d
Jason Schmidt <Ikkalyzte@users.noreply.github.com>
2014-10-22 10:47:38 -0400

    Added serial port test script for Arduino

---
+++ Arduino/Serial_port_test/Serial_port_test.ino
@@ -0,0 +1,16 @@
+
+
+
+void setup() {
+ //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  Serial.write("I exist");
+}
+
+void loop() {
+ //proceed normally
+}
+
--- server/backend/arduino_pull_script.rb
+++ server/backend/arduino_pull_script.rb
@@ -1,23 +1,28 @@
-require "serialport"
+require "/home/ikkalyzte/.rvm/gems/ruby-2.1.3/gems/serialport-1.3.1"
 
 #params for serial port
-port_str = SOME_PORT_NAME
+port_str = "ttyACM0"
 baud_rate = 9600
 data_bits = 8
 stop_bits = 1
 parity = SerialPort::NONE
-file_str = SOME_FILE_NAME
+file_str = "temp_data"
 
 time = Time.new
-file = IO.open(file_str,w)
+#file = IO.open(file_str,w)
 port = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
 
 #just read forever
+#while true do
+#   while (i = port.gets) do
+#     file.puts(i)
+#     file.puts(time.inspect)
+#   end
+#end
 while true do
-   while (i = port.gets) do
-     file.puts(i)
-     file.puts(time.inspect)
-   end
+  while (i=port.gets) do
+    puts(i)
+  end
 end
 
 port.close
